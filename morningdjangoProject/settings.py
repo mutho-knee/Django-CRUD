40efd6f0bc7eb2f769cddebcc4e3ecc67176de7c
mutho-knee <mwangimichelle911@gmail>
2023-03-02 10:38:56 +0300

    second commit

--- morningdjangoProject/settings.py
+++ morningdjangoProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap4',
+    'morningdjangoProject'
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,10 @@ STATICFILES_DIRS = [
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- morningdjangoProject/urls.py
+++ morningdjangoProject/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views as general_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.register, name='register')
+    path('', general_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('home/', general_views.home, name='home'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('add_product/', general_views.add_product, name='add_product'),
 ]

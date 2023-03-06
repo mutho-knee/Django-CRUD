9b79f6ca4c2a61ea711a060417849d46b870f3a4
mutho-knee <mwangimichelle911@gmail>
2023-03-06 10:36:11 +0300

    added feature

--- morningdjangoProject/settings.py
+++ morningdjangoProject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap4',
-    'morningdjangoProject'
+    'morningdjangoProject',
+    'django_daraja',
 ]
 
 MIDDLEWARE = [
